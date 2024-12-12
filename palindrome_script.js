10f76dfd6aeeb5b6bf081d71fcb58ca47bedcd30
AgentNAM <99711333+AgentNAM@users.noreply.github.com>
2024-12-12 10:14:33 -0700

    Add files via upload

--- palindrome_script.js
+++ palindrome_script.js
@@ -17,32 +17,44 @@ console.log("tag=" + checkPalindrome("tag"));
 console.log("A man a plan a canal Panama=" + checkPalindrome("A man a plan a canal Panama"));
 */
 
-// bContinue will track if the user wantes to keep entering words
-var bContinue = true;
-
-// loop to keep running the code for as long as the user wants
-do {
-    // get a word from the user
-    var userInput = prompt("Enter a word to test if it is a palindrome: ");
-
-    // test the variable that the user entered
-    var isPalindrome = checkPalindrome(userInput);
-
-    // show the user a message based on the true/false value returned from the function
-    // if isPalindrome is true, the if statement will run
-    if (isPalindrome){ // the same as if (isPalindrome == true){
-        alert(userInput + " is a palindrome!");
-    }
-    else { // if not a palindrome
-        alert(userInput + " is NOT a palindrome!");
-    }
-
-    // ask the user if they want to continue testing palindromes
-    var answer = prompt("Do you want to continue? (y/n)");
-
-    // test to see if they entered n
-    if (answer.toLowerCase() == "n"){
-        bContinue = false; // change bContinue to false so we can exit out of the loop
-    }
+function runPalindromeChecker(){
+    // bContinue will track if the user wantes to keep entering words
+    var bContinue = true;
     
-} while(bContinue); // while bContinue is true, keep running the loop
+    // loop to keep running the code for as long as the user wants
+    do {
+        // get a word from the user
+        var userInput = prompt("Enter a word to test if it is a palindrome: ");
+
+        // if the user pressed "cancel", then userInput will be null, so exit the loop
+        if (userInput == null){
+            break;
+        }
+
+        // test the variable that the user entered
+        var isPalindrome = checkPalindrome(userInput);
+    
+        // show the user a message based on the true/false value returned from the function
+        // if isPalindrome is true, the if statement will run
+        if (isPalindrome){ // the same as if (isPalindrome == true){
+            alert(userInput + " is a palindrome!");
+        }
+        else { // if not a palindrome
+            alert(userInput + " is NOT a palindrome!");
+        }
+    
+        // ask the user if they want to continue testing palindromes
+        var answer = prompt("Do you want to continue? (y/n)");
+    
+        // if the user pressed "cancel", then answer will be null, so exit the loop
+        if (answer == null){
+            break;
+        }
+
+        // test to see if they entered n
+        if (answer.toLowerCase() == "n"){
+            bContinue = false; // change bContinue to false so we can exit out of the loop
+        }
+        
+    } while(bContinue); // while bContinue is true, keep running the loop
+}
